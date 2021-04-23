fb1afc8a75de8ffa234383a89b2f70d37e8fb0b0
maxpocock <c19323561@mytudublin.ie>
2021-04-23 15:24:36 +0100

    projectStart

---
+++ java/src/c19323561/Project.java
@@ -0,0 +1,60 @@
+package c19323561;
+
+import ie.tudublin.Visual;
+
+public class Project extends Visual{
+    
+    public void settings()
+    {
+        //size(1024, 500);
+        fullScreen();
+
+        // Use this to make fullscreen and use P3D for 3D graphics
+        //fullScreen(P3D, SPAN); 
+    }
+
+    public void setup()
+    {
+        startMinim();
+                
+        // Call loadAudio to load an audio file to process 
+        loadAudio("heroplanet.mp3");   
+
+        
+        // Call this instead to read audio from the microphone
+        //startListening(); 
+        
+        wf = new WaveForm(this);
+        abv = new AudioBandsVisual(this);
+    }
+
+    public void keyPressed()
+    {
+        if (key == ' ')
+        {
+            getAudioPlayer().cue(0);
+            getAudioPlayer().play();
+        }
+    }
+
+    public void draw()
+    {
+        background(0);
+        try
+        {
+            // Call this if you want to use FFT data
+            calculateFFT(); 
+        }
+        catch(VisualException e)
+        {
+            e.printStackTrace();
+        }
+        // Call this is you want to use frequency bands
+        calculateFrequencyBands(); 
+
+        // Call this is you want to get the average amplitude
+        calculateAverageAmplitude();        
+        wf.render();
+        abv.render();
+    }
+}
