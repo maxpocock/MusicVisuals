4cf1586ca47a384384c032e63a2b2c5ce6dfd8a4
maxpocock <c19323561@mytudublin.ie>
2021-05-03 14:45:48 +0100

    BallMouseControls

--- java/src/c19323561/Project.java
+++ java/src/c19323561/Project.java
@@ -8,14 +8,14 @@ public class Project extends Visual
 {
     int selection = 1;
 
-    Rain[] r = new Rain[100];
+    Rain[] r = new Rain[60];
     Ball b;
     AudioPlayer ap;
     Sunset s;
 
     public void settings()
     {
-        size(1500, 800);
+        fullScreen();
     }
 
     public void setup()
@@ -23,9 +23,6 @@ public class Project extends Visual
         startMinim();
                 
         loadAudio("Cradles.mp3");
-        getAudioPlayer().cue(0);
-        getAudioPlayer().play();
-
         for(int i = 0; i < r.length; i++)
         {
             r[i] = new Rain(this);
@@ -69,11 +66,47 @@ public class Project extends Visual
         }
     }
 
+    @Override
     public void keyPressed()
     {
         if (keyCode >= '1' && keyCode <= '3')
         {
             selection = keyCode - '0';
         }
+
+        if (key == ' ')
+        {
+            getAudioPlayer().cue(0);
+            getAudioPlayer().play();
+        }
+
+        if (keyCode == '0')
+        {
+            b.x = width/2;
+            b.y = height/2;
+        }
+        else
+        {
+            b.x += b.dirX;
+            b.y += b.dirY;
+        }
+    }
+
+    @Override
+    public void mouseDragged()
+    {
+            b.x = mouseX;
+            b.y = mouseY;
+            b.dirX = 0;
+            b.dirY = 0;
+        
+    }
+
+    @Override
+    public void mouseClicked()
+    {
+        b.dirX = random(-4,4);
+        b.dirY = random(-4,4);
+        
     }
 }
